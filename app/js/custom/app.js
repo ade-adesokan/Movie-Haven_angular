58250b40a672948dff7cdc632cd5aa1997fd36e9
andela-aadesokan <adewunmi.adesokan@andela.co>
2014-12-20 18:37:55 +0100

    third commit

--- app/js/custom/app.js
+++ app/js/custom/app.js
@@ -1,15 +1,48 @@
 (function () {
   var movieHaven = angular.module('movieHaven', []);
-  movieHaven.controller('MovieDetailsController', function($scope, $http) {  
+  movieHaven.controller('MovieDetailsController', function($scope, $http, $sce) {  
 
+    $scope.enableSearch = function() {
+      $scope.hideIntro = true;
+      $scope.hideLoading = false;  
+      $scope.hideMovieNotFound = true;    
+    };
+    ($scope.disableSearch = function() {
+      $scope.hideLoading = true;
+      $scope.hideMovieInfo = true;
+      $scope.hideMovieNotFound = true;
+   })();
+    
     
     $scope.findMovie = function(){
-      var url = 'http://www.myapifilms.com/imdb?callback=JSON_CALLBACK&title=' + $scope.movieName + '&format=JSONP'; 
+      $scope.enableSearch();
+      var url = 'http://www.myapifilms.com/imdb?callback=JSON_CALLBACK&title=' + $scope.movieName + '&format=JSONP&actors=S&trailer=1'; 
       $http.jsonp(url).success(function (data) {
         $scope.movieInfo = data;
+
+        if( $scope.movieInfo.message === "Movie not found" || $scope.movieInfo.message === "IMDB Id and Title can not be empty" ) {
+          $scope.hideMovieNotFound = false;
+
+        } else {
+
+          if(Object.keys($scope.movieInfo[0].trailer).length === 0) {
+            $scope.video = "image/noMedia.jpg"
+          } else {
+
+            $scope.video = $scope.movieInfo[0].trailer.videoURL + "/imdb/embed?autoplay=false&width=480";
+            $scope.video = $sce.trustAsResourceUrl($scope.video);
+
+            console.log($scope.video);
+          }
+        }
         console.log($scope.movieInfo[0].countries);
+        $scope.disableSearch();
+        $scope.movieName = "";
+        $scope.hideMovieInfo = false;
       });
-    };   
+    };  
+
+
 
 
   });
